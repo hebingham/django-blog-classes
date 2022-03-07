8167643c0095d5972407a3b153fa899740928035
hebingham <hebingham77@gmail.com>
2022-03-07 13:38:32 -0800

    added DATABASE_URL setting and conn setting to heroku settings

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -2,6 +2,8 @@ import os
 
 import dj_database_url
 
+import psycopg2
+
 from .settings import *
 
 DATABASES = {
@@ -20,3 +22,7 @@ MIDDLEWARE = (
     "whitenoise.middleware.WhiteNoiseMiddleware",
     *MIDDLEWARE,
 )
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
